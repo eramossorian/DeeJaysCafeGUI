addf643e94731c56f7ec3b1e846adc9252fa07d1
eramossorian <eramos-soriano@csub.edu>
2021-12-08 07:07:28 -0800

    GUI Update

---
+++ home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Deejay's Cafe - Home</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<header>
+		<img class="logo" src="djclogo.png" alt="logo" height="70px">
+		<nav>
+			<ul class="nav_links">
+				<li><a href="home.php">Home</a></li>
+				<li><a href="#">Menu & Recipes</a></li>
+				<li><a href="#">Order Online</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="image">
+	<div class="container-1">
+		<div class="box-1">
+			<h3>Enjoy the best coffee in town!</h3>
+			<p>Check out our selection of our world class beverages</p>
+			<a href="#" class="button">Order Here</a>
+		</div>	
+	</div>
+	</div>
+	
+	<div class="container-2">
+	  <div class="side-1">
+		<ul class="info_links">
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Admin Login</a></li>
+		</ul>
+	  </div>
+	  <div class="side-2">
+		<ul class="hours">
+			<li style="font-weight: bolder;">Hours:</li>
+			<li>Monday: 6am-9pm</li>
+			<li>Tuesday: 6am-9pm</li>
+			<li>Wednesday: 6am-9pm</li>
+			<li>Thursday: 6am-9pm</li>
+			<li>Friday: 6am-9pm</li>
+			<li>Saturday: 7am-6pm</li>
+			<li>Sunday: 7am-6pm</li>
+		</ul>
+	   </div>
+	</div>
+    <footer>
+    	<p>© 2021 DeeJay's Cafe all rights reserved.</p>
+	</footer>
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Deejay's Cafe - Menu & Recipes</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<header>
+		<img class="logo" src="djclogo.png" alt="logo" height="70px">
+		<nav>
+			<ul class="nav_links">
+				<li><a href="home.php">Home</a></li>
+				<li><a href="#">Menu & Recipes</a></li>
+				<li><a href="#">Order Online</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="container-3">
+		<div class="menu-1">
+
+		</div>	
+	</div>
+	
+    <footer class="alt-footer">
+    	<p>© 2021 DeeJay's Cafe all rights reserved.</p>
+	</footer>
+</body>
+
+</html>
